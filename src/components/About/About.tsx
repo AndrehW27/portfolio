365d960c311cc484efd2183ff1dbeb58ace932f4
AndrehW27 <andrehwillian@hotmail.com>
2025-11-05 13:03:18 -0300

    responsivity adjust

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -40,7 +40,7 @@ function About() {
     <>
       <section id="about" className='bg-background border-accent w-screen flex flex-col items-center justify-start pb-20'>
 
-        <div className='flex justify-center items-end h-screen w-full border-red-500'>
+        <div className='flex justify-center items-end h-screen w-full border-red-500 text-sm sm:text-base'>
           <div className='flex flex-col justify-end items-start h-160 w-70 sm:w-160 border-blue-500 pl-8'>
             <div data-aos="fade-right" className='text-text flex mr-5 mb-5'>
               <div className='text-accent w-10 h-10 mr-2'>
@@ -80,61 +80,61 @@ function About() {
           <div className='w-40 sm:w-100 h-full border-red-500 flex flex-col justify-start items-center'>
             <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
               <div>
-                <img data-aos="fade-right" className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#3594d2] hover:shadow-[0_0_40px_#3594d2] transition duration-300 ease-in-out' src={logic} />
+                <img data-aos="fade-right" className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#3594d2] hover:shadow-[0_0_40px_#3594d2] transition duration-300 ease-in-out' src={logic} />
               </div>
             </div>
             <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
-              <div data-aos="fade-right" className='flex align-center justify-center gap-4'>
-                <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#82aee3] hover:shadow-[0_0_40px_#82aee3] transition duration-300 ease-in-out' src={settings} />
-                <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#2f94f6] hover:shadow-[0_0_40px_#2f94f6] transition duration-300 ease-in-out' src={english} />
+              <div data-aos="fade-right" className='flex align-center justify-center gap-1 sm:gap-4'>
+                <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#82aee3] hover:shadow-[0_0_40px_#82aee3] transition duration-300 ease-in-out' src={settings} />
+                <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#2f94f6] hover:shadow-[0_0_40px_#2f94f6] transition duration-300 ease-in-out' src={english} />
               </div>
             </div>
             <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
-              <div data-aos="fade-right" className='flex align-center justify-center gap-4'>
-                <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f0db4f] hover:shadow-[0_0_40px_#f0db4f] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-                <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f16529] hover:shadow-[0_0_40px_#f16529] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-                <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#31a5d9] hover:shadow-[0_0_40px_#31a5d9] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+              <div data-aos="fade-right" className='flex align-center justify-center gap-1 sm:gap-4'>
+                <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f0db4f] hover:shadow-[0_0_40px_#f0db4f] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f16529] hover:shadow-[0_0_40px_#f16529] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#31a5d9] hover:shadow-[0_0_40px_#31a5d9] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
               </div>
             </div>
             <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
               <div data-aos="fade-right" >
-                <div className='flex align-center justify-center gap-4'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#ea23a4] hover:shadow-[0_0_40px_#ea23a4] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#d32a2d] hover:shadow-[0_0_40px_#d32a2d] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#a9bfd1] hover:shadow-[0_0_40px_#a9bfd1] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" />
+                <div className='flex align-center justify-center gap-1 sm:gap-4'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#ea23a4] hover:shadow-[0_0_40px_#ea23a4] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#d32a2d] hover:shadow-[0_0_40px_#d32a2d] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#a9bfd1] hover:shadow-[0_0_40px_#a9bfd1] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" />
                 </div>
-                <div className='flex align-center justify-center gap-4 mt-5'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#6762a6] hover:shadow-[0_0_40px_#6762a6] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/heroku/heroku-plain-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#4f982e] hover:shadow-[0_0_40px_#4f982e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#8e13fd] hover:shadow-[0_0_40px_#8e13fd]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
-                </div>
-              </div>
-            </div>
-            <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
-              <div data-aos="fade-right">
-                <div className='flex align-center justify-center gap-4'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#00a1e0] hover:shadow-[0_0_40px_#00a1e0] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/salesforce/salesforce-original.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#ff9900] hover:shadow-[0_0_40px_#ff9900] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#5b00d1] hover:shadow-[0_0_40px_#5b00d1] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/insomnia/insomnia-original.svg" />
-                </div>
-                <div className='flex align-center justify-center gap-4 mt-5'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 bg-text border border-text-muted p-2 rounded-md shadow-[0_0_20px_#e0e0e6] hover:shadow-[0_0_40px_#e0e0e6]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/splunk/splunk-original-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#007acc] hover:shadow-[0_0_40px_#007acc] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f24e1e] hover:shadow-[0_0_40px_#f24e1e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                <div className='flex align-center justify-center gap-1 sm:gap-4 mt-5'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#6762a6] hover:shadow-[0_0_40px_#6762a6] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/heroku/heroku-plain-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#4f982e] hover:shadow-[0_0_40px_#4f982e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#8e13fd] hover:shadow-[0_0_40px_#8e13fd]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
                 </div>
               </div>
             </div>
             <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
               <div data-aos="fade-right">
-                <div className='flex align-center justify-center gap-4'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#61dafb] hover:shadow-[0_0_40px_#61dafb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 bg-text border border-text-muted p-2 rounded-md shadow-[0_0_20px_#e0e0e6] hover:shadow-[0_0_40px_#e0e0e6]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#5fa04e] hover:shadow-[0_0_40px_#5fa04e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+                <div className='flex align-center justify-center gap-1 sm:gap-4'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#00a1e0] hover:shadow-[0_0_40px_#00a1e0] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/salesforce/salesforce-original.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#ff9900] hover:shadow-[0_0_40px_#ff9900] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#5b00d1] hover:shadow-[0_0_40px_#5b00d1] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/insomnia/insomnia-original.svg" />
                 </div>
-                <div className='flex align-center justify-center gap-4 mt-5'>
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#4faa41] hover:shadow-[0_0_40px_#4faa41] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-plain-wordmark.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#29acdb] hover:shadow-[0_0_40px_#29acdb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" />
-                  <img className='h-8 w-8 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#29acdb] hover:shadow-[0_0_40px_#29acdb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                <div className='flex align-center justify-center gap-1 sm:gap-4 mt-5'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 bg-text border border-text-muted p-2 rounded-md shadow-[0_0_20px_#e0e0e6] hover:shadow-[0_0_40px_#e0e0e6]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/splunk/splunk-original-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#007acc] hover:shadow-[0_0_40px_#007acc] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#f24e1e] hover:shadow-[0_0_40px_#f24e1e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                </div>
+              </div>
+            </div>
+            <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
+              <div data-aos="fade-right">
+                <div className='flex align-center justify-center gap-1 sm:gap-4'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#61dafb] hover:shadow-[0_0_40px_#61dafb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 bg-text border border-text-muted p-2 rounded-md shadow-[0_0_20px_#e0e0e6] hover:shadow-[0_0_40px_#e0e0e6]  transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#5fa04e] hover:shadow-[0_0_40px_#5fa04e] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+                </div>
+                <div className='flex align-center justify-center gap-1 sm:gap-4 mt-5'>
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#4faa41] hover:shadow-[0_0_40px_#4faa41] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-plain-wordmark.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#29acdb] hover:shadow-[0_0_40px_#29acdb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" />
+                  <img className='h-11 w-11 sm:h-15 sm:w-15 border border-text-muted p-2 rounded-md shadow-[0_0_20px_#29acdb] hover:shadow-[0_0_40px_#29acdb] transition duration-300 ease-in-out' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
                 </div>
               </div>
             </div>
@@ -184,7 +184,7 @@ function About() {
 
           </div>
 
-          <div className='text-text w-40 px-3 text-xs sm:w-100 h-full  border-red-500 flex flex-col justify-start items-center'>
+          <div className='text-text w-40 px-3 text-sm sm:text-base sm:w-100 h-full  border-red-500 flex flex-col justify-start items-center'>
             <div className='w-40 px-3 sm:w-100 h-50 border-orange-500 flex justify-center items-start'>
               <div data-aos="fade-left">
                 <h1 className='text-accent font-bold'>University Federal of ABC - 2012</h1>
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import './Contact.css'
+
 // import { Laptop } from "lucide-react";
 // import { Menu } from "lucide-react";
 
@@ -6,9 +7,15 @@ function Contact() {
 
   return (
     <>
-    <section id="contact" className='bg-background h-screen w-screen flex flex-col items-center justify-center'>
-      <h1 className='text-text'>Contact</h1>
-    </section>
+      <section id="contact" className='bg-background h-screen w-screen flex flex-col items-center justify-center'>
+        <h1 className='text-text'>Contact</h1>
+    <div className="min-h-screen bg-white dark:bg-gray-900 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100 mb-4">
+        React + Tailwind Dark Mode
+      </h1>
+      {/* <ThemeToggle /> */}
+    </div>
+      </section>
     </>
   )
 }
