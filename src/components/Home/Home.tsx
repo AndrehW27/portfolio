511366b7502addb01d094ac84c7788e10b2e6858
AndrehW27 <andrehwillian@hotmail.com>
2025-11-10 19:50:24 -0300

    aw smaller

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -110,7 +110,7 @@ function Hero({ theme }: HeroProps) {
           >
 
             <h1 className="text-1xl sm:text-2xl font-bold mt-4">{t("home.welcome")}</h1>
-            <h1 className="text-3xl sm:text-5xl font-bold mt-6 py-3 px-6 rounded-2xl bg-linear-to-r from-surface to-accent white:from-text-dark white:to-light-blue  white:text-text shadow-[0_0_40px_#7C3AED] white:shadow-[0_0_40px_#415ff5]">{t("home.title")}</h1>
+            <h1 className="text-2xl sm:text-5xl font-bold mt-6 py-3 px-6 rounded-2xl bg-linear-to-r from-surface to-accent white:from-text-dark white:to-light-blue  white:text-text shadow-[0_0_40px_#7C3AED] white:shadow-[0_0_40px_#415ff5]">{t("home.title")}</h1>
 
             <h3 className="typing text-2xl sm:text-4xl border-r-3 border-accent white:border-light-blue text-accent white:text-light-blue font-bold mt-6 mb-10">
               {t("home.subtitle")}
