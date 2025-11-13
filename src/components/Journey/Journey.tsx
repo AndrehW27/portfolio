494a103d2b75a2271edb5154fb2a21c76e393a08
AndrehW27 <andrehwillian@hotmail.com>
2025-11-13 14:33:33 -0300

    journey h40

--- src/components/Journey/Journey.tsx
+++ src/components/Journey/Journey.tsx
@@ -32,33 +32,33 @@ function Journey() {
                 <div className='relative  pb-30 white:text-dark-gray  border-yellow-300 text-xs w-[70vw] flex items-center justify-center mt-10'>
 
                     <div className='w-35 sm:text-base sm:w-100 border-red-500 flex flex-col justify-start items-center'>
-                        <div className='w-40 sm:w-100 h-50 border-blue-500 flex justify-end items-start'>
+                        <div className='w-40 sm:w-100 h-40 border-blue-500 flex justify-end items-start'>
                             <div>
                                 <img data-aos="fade-right" className=' icons shadow-[0_0_10px_#3594d2] white:shadow-[0_0_10px_#3594d2] transition' src={logic} />
                             </div>
                         </div>
-                        <div className='w-40 pl-6 sm:w-100 h-50 border-orange-500  flex justify-center items-start'>
+                        <div className='w-40 pl-6 sm:w-100 h-40 border-orange-500  flex justify-center items-start'>
                             <div data-aos="fade-right">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.wsu")}</h1>
                                 <p>{t("about.wsu.text")}</p>
                             </div>
                         </div>
 
-                        <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
+                        <div className='w-40 sm:w-100 h-40 border-orange-500 flex justify-end items-start'>
                             <div data-aos="fade-right" className='flex align-center justify-center gap-2 sm:gap-4'>
                                 <img className='icons shadow-[0_0_10px_#f0db4f] white:shadow-[0_0_10px_#f0db4f] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
                                 <img className='icons shadow-[0_0_10px_#f16529] white:shadow-[0_0_10px_#f16529] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
                                 <img className='icons shadow-[0_0_10px_#31a5d9] white:shadow-[0_0_10px_#31a5d9] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
                             </div>
                         </div>
-                        <div className='w-40 pl-6 sm:w-100 h-50  border-orange-500 flex justify-center items-start'>
+                        <div className='w-40 pl-6 sm:w-100 h-40  border-orange-500 flex justify-center items-start'>
                             <div data-aos="fade-right">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.gen")}</h1>
                                 <p>{t("about.gen.text")}</p>
                             </div>
                         </div>
 
-                        <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-end items-start'>
+                        <div className='w-40 sm:w-100 h-40 border-orange-500 flex justify-end items-start'>
                             <div data-aos="fade-right">
                                 <div className='flex align-center justify-center gap-2 sm:gap-4'>
                                     <img className='icons shadow-[0_0_10px_#00a1e0] white:shadow-[0_0_10px_#00a1e0] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/salesforce/salesforce-original.svg" />
@@ -72,7 +72,7 @@ function Journey() {
                                 </div>
                             </div>
                         </div>
-                        <div className='w-40 pl-6 sm:w-100 h-50  border-orange-500 flex justify-center items-start'>
+                        <div className='w-40 pl-6 sm:w-100 h-40  border-orange-500 flex justify-center items-start'>
                             <div data-aos="fade-right">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.puc")}</h1>
                                 <p>{t("about.puc.text")}</p>
@@ -82,42 +82,42 @@ function Journey() {
                     </div>
 
                     <div className='w-20 sm:w-50 border-red-500 flex flex-col justify-start items-center'>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <School className='2' />
                             </div>
                             <div data-aos="fade-up" className='w-1 h-40 bg-accent white:bg-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] transition'>
                             </div>
                         </div>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <GraduationCap className='2' />
                             </div>
                             <div data-aos="fade-up" className='w-1 h-40 bg-accent white:bg-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] transition'>
                             </div>
                         </div>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <University className='2' />
                             </div>
                             <div data-aos="fade-up" className='w-1 h-40 bg-accent white:bg-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] transition'>
                             </div>
                         </div>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <Factory className='2' />
                             </div>
                             <div data-aos="fade-up" className='w-1 h-40 bg-accent white:bg-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] transition'>
                             </div>
                         </div>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <Landmark className='2' />
                             </div>
                             <div data-aos="fade-up" className='w-1 h-40 bg-accent white:bg-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] transition'>
                             </div>
                         </div>
-                        <div className='w-50 h-50 border-orange-500 flex flex-col justify-start items-center'>
+                        <div className='w-50 h-40 border-orange-500 flex flex-col justify-start items-center'>
                             <div data-aos="fade-up-right" className='h-9 w-9 sm:h-12 sm:w-12 border-3 border-accent white:border-light-blue shadow-[0_0_30px_#7C3AED] white:shadow-[0_0_30px_#6594fc] rounded-full flex justify-center items-center 1 transition'>
                                 <Rocket className='2' />
                             </div>
@@ -126,26 +126,26 @@ function Journey() {
                     </div>
 
                     <div className='w-35 px-6 sm:text-base sm:w-100 border-red-500 flex flex-col justify-start items-center'>
-                        <div className='w-40 pr-6 sm:w-100 h-50  border-orange-500 flex justify-center items-start'>
+                        <div className='w-40 pr-6 sm:w-100 h-40  border-orange-500 flex justify-center items-start'>
                             <div data-aos="fade-left">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.ufabc")}</h1>
                                 <p>{t("about.ufabc.text")}</p>
                             </div>
                         </div>
 
-                        <div className='w-40 sm:w-100 h-50  border-orange-500 flex justify-start items-start'>
+                        <div className='w-40 sm:w-100 h-40  border-orange-500 flex justify-start items-start'>
                             <div data-aos="fade-left" className='flex align-center justify-center gap-2 sm:gap-4'>
                                 <img className='icons shadow-[0_0_10px_#82aee3] white:shadow-[0_0_10px_#82aee3] transition' src={settings} />
                                 <img className='icons shadow-[0_0_10px_#2f94f6] white:shadow-[0_0_10px_#2f94f6] transition' src={english} />
                             </div>
                         </div>
-                        <div className='w-40 pr-6 sm:w-100 h-50  border-orange-500 flex justify-center items-start'>
+                        <div className='w-40 pr-6 sm:w-100 h-40  border-orange-500 flex justify-center items-start'>
                             <div data-aos="fade-left">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.gm")}</h1>
                                 <p>{t("about.gm.text")}</p>
                             </div>
                         </div>
-                        <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-start items-start'>
+                        <div className='w-40 sm:w-100 h-40 border-orange-500 flex justify-start items-start'>
                             <div data-aos="fade-left" >
                                 <div className='flex align-center justify-center gap-2 sm:gap-4'>
                                     <img className='icons shadow-[0_0_10px_#ea23a4] white:shadow-[0_0_10px_#ea23a4] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg" />
@@ -160,14 +160,14 @@ function Journey() {
                             </div>
                         </div>
 
-                        <div className='w-40 pr-6 sm:w-100 h-50  border-orange-500 flex justify-center items-start'>
+                        <div className='w-40 pr-6 sm:w-100 h-40  border-orange-500 flex justify-center items-start'>
                             <div data-aos="fade-left">
                                 <h1 className='text-accent white:text-light-blue font-bold'>{t("about.itau")}</h1>
                                 <p>{t("about.itau.text")}</p>
                             </div>
                         </div>
 
-                        <div className='w-40 sm:w-100 h-50 border-orange-500 flex justify-start items-start'>
+                        <div className='w-40 sm:w-100 h-40 border-orange-500 flex justify-start items-start'>
                             <div data-aos="fade-left">
                                 <div className='flex align-center justify-center gap-2 sm:gap-4'>
                                     <img className='icons shadow-[0_0_10px_#61dafb] white:shadow-[0_0_10px_#61dafb] transition' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
