950c7a1bd60698cab2ed24bd7799dc9ab5fe687d
AndrehW27 <andrehwillian@hotmail.com>
2025-11-14 01:26:08 -0300

    project h80

--- src/components/Test/Teste.tsx
+++ src/components/Test/Teste.tsx
@@ -8,7 +8,7 @@ export default function Teste() {
     <>
       <section id="test" className="relative h-[100dvh] w-dvw bg-background flex items-center justify-center">
 
-        <div className="absolute top-15 bg-cover h-[90dvh] w-[90dvw] bg-center bg-no-repeat rounded-3xl  border-surface" style={{ backgroundImage: `url(${bg})` }}></div>
+        <div className="absolute top-15 bg-cover h-[85dvh] w-[90dvw] bg-center bg-no-repeat rounded-3xl  border-surface" style={{ backgroundImage: `url(${bg})` }}></div>
         {/* <div className="absolute bottom-0 left-0 w-full h-[100dvh] bg-[linear-gradient(to_top,#111_30%,transparent_70%)]">
 
           <div className='absolute bottom-20 left-20 w-40 sm:w-100 h-40 border-orange-500 flex justify-end items-start'>
@@ -22,16 +22,16 @@ export default function Teste() {
           <h1 className="absolute bottom-20 left-10 text-text w-70">Comprehensive Fullstack app with database, secure authentication, and ChatGPT-powered AI integration.</h1>
 
         </div> */}
-        <div className="absolute top-15 bg-cover h-[90dvh] w-[90dvw] rounded-3xl  bg-black/75 text-white/99 ">
+        <div className="absolute top-15 bg-cover h-[85dvh] w-[90dvw] rounded-3xl  bg-black/75 text-white/99 ">
           <div className="absolute top-8 left-6 text-3xl font-bold">ZeraMeta </div>
           <div className="absolute top-20 left-6 text-sm w-65">Comprehensive Fullstack app with database, secure authentication, and ChatGPT-powered AI integration.</div>
-          <div className="absolute top-150 left-6 text-sm bg-white/99 text-black/80 px-5 py-2 rounded-2xl">Code</div>
-          <div className="absolute top-150 left-33 text-sm bg-white/99 text-black/80 px-5 py-2 rounded-2xl">Demo</div>
+          <div className="absolute top-140 left-6 text-sm bg-white/99 text-black/80 px-5 py-2 rounded-2xl">Code</div>
+          <div className="absolute top-140 left-33 text-sm bg-white/99 text-black/80 px-5 py-2 rounded-2xl">Demo</div>
 
           {/* <div className="absolute top-11 left-48 text-3xl"><i className="devicon-angular-plain"></i></div>
           <div className="absolute top-11 left-60 text-3xl"><i className="devicon-mongodb-plain-wordmark"></i></div>
           <div className="absolute top-11 left-72 text-3xl"><i className="devicon-nodejs-plain-wordmark"></i></div> */}
-{/* 
+          {/* 
           <div className="absolute top-22 left-10 text-3xl"><i className="devicon-angular-plain"></i></div>
           <div className="absolute top-22 left-25 text-3xl"><i className="devicon-mongodb-plain-wordmark"></i></div>
           <div className="absolute top-22 left-40 text-3xl"><i className="devicon-nodejs-plain-wordmark"></i></div> */}
@@ -40,15 +40,13 @@ export default function Teste() {
           <div className="absolute top-40 left-23 text-3xl"><i className="devicon-mongodb-plain-wordmark"></i></div>
           <div className="absolute top-40 left-38 text-3xl"><i className="devicon-nodejs-plain-wordmark"></i></div>
 
-          <div className="absolute top-150 left-70 text-3xl"><i className="devicon-nodejs-plain"></i></div>
+          <div className="absolute top-140 right-6 text-3xl"><i className="devicon-nodejs-plain"></i></div>
 
-          <div className="absolute top-70 right-70 text-3xl text-white/30"><i className="devicon-nodejs-plain"></i></div>
-          <div className="absolute top-70 left-70 text-3xl text-white/30"><i className="devicon-nodejs-plain"></i></div>
-
-
-
+          <div className="absolute top-70 right-2 text-3xl text-white/99">›</div>
+          <div className="absolute top-70 left-2 text-3xl text-white/99">‹</div>
 
         </div>
+        
 
 
 
